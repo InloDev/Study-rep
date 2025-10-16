078a1059a5c20196af4b00c1ed265b2f3a5a502e
Static <vieru.kir@gmail.com>
2025-10-16 20:29:51 +0300

    add test2

---
+++ src/Test2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Обработка исключений при вводе
+    Программа запрашивает число.
+
+* Если введено не число → ошибка и повторный ввод
+* Если число корректное → вывести его удвоенное значение
+*/
+
+while (true)
+{
+    Console.Write("Введите число: ");
+    var input = Console.ReadLine();
+    if (int.TryParse(input, out var number))
+    {
+        Console.WriteLine($"Удвоенное число: {number * number}");
+        break;
+    }
+
+    Console.WriteLine("Ошибка ввода!");
+}
