eda09f57cdc0254e551f87c9e15c537284be0dd9
Static <vieru.kir@gmail.com>
2025-10-16 20:43:49 +0300

    add test3

---
+++ src/Test3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Отгадайте число!!! Игра начинается!!!");
+var random = new Random();
+var randomNumber = random.Next(0, 100);
+while (true)
+{
+    Console.Write("Введите число: ");
+    var userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber == randomNumber)
+    {
+        Console.WriteLine("Ура Вы угадали загаданное число!!!");
+        break;
+    }
+
+    if (userNumber < randomNumber) Console.WriteLine("Загаданное число больше");
+    if (userNumber > randomNumber) Console.WriteLine("Загаданное число меньше");
+}
