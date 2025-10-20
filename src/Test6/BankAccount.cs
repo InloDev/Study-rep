a0617c1935e1cc6125f7421a00beea61eba857f8
Static <vieru.kir@gmail.com>
2025-10-20 11:00:15 +0300

    Refactoring

---
+++ src/Test6/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿internal class BankAccount
+{
+    private readonly int _id;
+    private int _balance;
+
+    public BankAccount(int id)
+    {
+        _id = id;
+        _balance = 0;
+    }
+
+    public BankAccount(int id, int balance)
+    {
+        _id = id;
+        if (balance > 0)
+            _balance = balance;
+    }
+
+    public void Deposit(int amount)
+    {
+        if (amount <= 0) Console.WriteLine("Введено недопустимое значение.");
+        _balance += amount;
+        Console.WriteLine($"Счет пополнен на {amount}. Остаток на счете: {_balance} ");
+    }
+
+    public void Withdraw(int amount)
+    {
+        if (amount > 0 || _balance - amount > 0)
+        {
+            _balance -= amount;
+            Console.WriteLine($"С счета снято {amount}. Остаток на счете: {_balance}");
+        }
+        else
+        {
+            Console.WriteLine("Не удалось снять средсва. Недостаточно средств.");
+        }
+    }
+
+    public void Transfer(BankAccount bankAccount, int amount)
+    {
+        if (_balance - amount > 0)
+        {
+            _balance -= amount;
+            bankAccount.Deposit(amount);
+            Console.WriteLine($"На счет {bankAccount._id} переведенно {amount}");
+        }
+        else
+        {
+            Console.WriteLine("Не удалось перевести средства. Недостаточно средств.");
+        }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Остаток на счете: {_balance}");
+    }
+}
--- src/Test6/Program.cs
+++ src/Test6/Program.cs
@@ -9,71 +9,11 @@
   Баланс не может быть отрицательным.
 */
 
-var account1 = new BankAccount(00001, 250);
+var account1 = new BankAccount(10001, 250);
 account1.Deposit(1000);
 account1.Withdraw(1100);
 
-var account2 = new BankAccount(00002);
+var account2 = new BankAccount(10002);
 account1.Transfer(account2, 100);
 account1.Transfer(account2, 100);
 account2.Print();
-
-internal class BankAccount
-{
-    private readonly int _id;
-    private int _balance;
-
-    public BankAccount(int id)
-    {
-        _id = id;
-        _balance = 0;
-    }
-
-    public BankAccount(int id, int balance)
-    {
-        _id = id;
-        if (balance > 0)
-            _balance = balance;
-    }
-
-    public void Deposit(int amount)
-    {
-        if (amount > 0)
-        {
-            _balance += amount;
-            Console.WriteLine($"Счет пополнен на {amount}. Остаток на счете: {_balance} ");
-        }
-    }
-
-    public void Withdraw(int amount)
-    {
-        if (amount > 0 || _balance - amount > 0)
-        {
-            _balance -= amount;
-            Console.WriteLine($"С счета снято {amount}. Остаток на счете: {_balance}");
-        }
-        else
-        {
-            Console.WriteLine("Не удалось снять средсва. Недостаточно средств.");
-        }
-    }
-
-    public void Transfer(BankAccount bankAccount, int amount)
-    {
-        if (_balance - amount > 0)
-        {
-            _balance -= amount;
-            bankAccount.Deposit(amount);
-            Console.WriteLine($"На счет {bankAccount._id} переведенно {amount}");
-        }
-        else
-        {
-            Console.WriteLine("Не удалось перевести средства. Недостаточно средств.");
-        }
-    }
-
-    public void Print()
-    {
-        Console.WriteLine($"Остаток на счете: {_balance}");
-    }
-}
