a2e658567bfcf826b3e94571adf8e61c095e5ee7
Static <vieru.kir@gmail.com>
2025-10-20 10:56:34 +0300

    Refactoring

--- src/Test5/Program.cs
+++ src/Test5/Program.cs
@@ -13,34 +13,3 @@ tom.AddGrade(5);
 tom.AddGrades(grades);
 tom.PrintAverageGrades();
 tom.PrintInfo();
-
-
-internal class Student(string name)
-{
-    private readonly List<int?> _grades = new();
-
-    public void AddGrade(int? grade)
-    {
-        if (grade == null)
-            Console.WriteLine("Недопустимые значения");
-
-        _grades.Add(grade);
-    }
-
-    public void AddGrades(List<int?> grades)
-    {
-        _grades.AddRange(grades);
-    }
-
-    public void PrintAverageGrades()
-    {
-        Console.WriteLine($"Average grade: {_grades.Average()}");
-    }
-
-    public void PrintInfo()
-    {
-        Console.WriteLine($"Name: {name}");
-        Console.Write("Grades: ");
-        foreach (var grade in _grades) Console.Write($"{grade} ");
-    }
-}
