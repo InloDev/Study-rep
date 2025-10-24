9b934fe63071e24c85c8d2fe528d84bcc22ce46f
InloDev <vieru.kir@gmail.com>
2025-10-24 12:52:16 +0300

    add test 4 (#3)
    * add test 4
    
    * refactoring

---
+++ src/test4/Program.cs
@@ -0,0 +1,20 @@
+﻿var repeatOperation = 'Y';
+while (repeatOperation is 'Y' or 'y')
+{
+    Console.Write("Введите 1-е число: ");
+    var firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите 2-е число: ");
+    var secondNumber = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Выберите операцию (+, -, *, /): ");
+    var selectionOperation = Convert.ToChar(Console.ReadLine() ?? string.Empty);
+    switch (selectionOperation)
+    {
+        case '+': Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}"); break;
+        case '-': Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}"); break;
+        case '*': Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}"); break;
+        case '/': Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}"); break;
+    }
+
+    Console.WriteLine("Желаете продолжить? (Y/N)");
+    repeatOperation = Convert.ToChar(Console.ReadLine() ?? string.Empty);
+}
