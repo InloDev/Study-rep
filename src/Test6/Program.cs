5277db5525ebe5d3297c9780571251f3dded08ad
InloDev <vieru.kir@gmail.com>
2025-10-24 13:03:20 +0300

    add test6 (#5)
    * add test6
    
    * Refactoring

---
+++ src/Test6/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+6. Класс “Банк / Счёт”
+Класс BankAccount с балансом.
+Методы:
+
+* Deposit() — пополнить
+* Withdraw() — снять (если хватает средств)
+* Transfer() — перевести на другой счёт
+  Баланс не может быть отрицательным.
+*/
+
+var account1 = new BankAccount(10001, 250);
+account1.Deposit(1000);
+account1.Withdraw(1100);
+
+var account2 = new BankAccount(10002);
+account1.Transfer(account2, 100);
+account1.Transfer(account2, 100);
+account2.Print();
