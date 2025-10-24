01c5fdcb6bd942e86591f6d8b2706ed9fcc78d8d
InloDev <vieru.kir@gmail.com>
2025-10-24 12:57:14 +0300

    add test5 (#4)
    * add test5
    
    * Refactoring

---
+++ src/Test5/Student.cs
@@ -0,0 +1,29 @@
+﻿internal class Student(string name)
+{
+    private readonly List<int?> _grades = new();
+
+    public void AddGrade(int? grade)
+    {
+        if (grade is null)
+            Console.WriteLine("Недопустимые значения");
+
+        _grades.Add(grade);
+    }
+
+    public void AddGrades(List<int?> grades)
+    {
+        _grades.AddRange(grades);
+    }
+
+    public void PrintAverageGrades()
+    {
+        Console.WriteLine($"Average grade: {_grades.Average()}");
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Name: {name}");
+        Console.Write("Grades: ");
+        foreach (var grade in _grades) Console.Write($"{grade} ");
+    }
+}
