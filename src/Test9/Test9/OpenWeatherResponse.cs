bde1d6e95be149f953e98aac6279384d25bb7dfe
Static <vieru.kir@gmail.com>
2025-10-28 16:44:44 +0200

    Init Test9

---
+++ src/Test9/Test9/OpenWeatherResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace Test9;
+
+public class OpenWeatherResponse
+{
+    public string? Name { get; set; } //city name
+    public List<WeatherDescription>? Weather { get; set; }
+    public double Temp { get; set; }
+    public double WindSpeed { get; set; }
+
+    public class WeatherDescription
+    {
+        public string? Description { get; set; }
+    }
+
+    public class MainInfo
+    {
+        public double Temp { get; set; }
+    }
+
+    public class WindInfo
+    {
+        public double Speed { get; set; }
+    }
+}
---
+++ src/Test9/Test9/Program.cs
@@ -0,0 +1,8 @@
+﻿//Console.WriteLine("Введите название города: ");
+//var cityName = Console.ReadLine();
+//var apiKey = "65eed83ce86219a7eba772b95cccf376";
+
+using Test9;
+
+var cityName = "London";
+var weather = await Infrastructure(cityName);
